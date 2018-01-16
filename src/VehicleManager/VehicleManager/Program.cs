4cbbd75dc1baf652b605fbec8c50f6163c5d0e7a
jeffa <jeffa00@gmail.com>
2018-01-16 09:40:50 -0500

    Initial project creation

---
+++ src/VehicleManager/VehicleManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
